2d4e3ce44fa684b8d806646ed30e533a271e3ff5
Michael Wilson <mike@mdwn.dev>
2025-01-23 01:12:36 -0500

    fix: Fix hardcoded DMX universe. (#59)
    The DMX universe playback was using a hardcoded universe number.
    This has been corrected.

--- src/dmx/universe.rs
+++ src/dmx/universe.rs
@@ -1,4 +1,4 @@
-// Copyright (C) 2024 Michael Wilson <mike@mdwn.dev>
+// Copyright (C) 2025 Michael Wilson <mike@mdwn.dev>
 //
 // This program is free software: you can redistribute it and/or modify it under
 // the terms of the GNU General Public License as published by the Free Software
@@ -141,7 +141,7 @@ impl Universe {
                     max_channels.load(Ordering::Relaxed),
                     &mut buffer,
                 ) {
-                    if let Err(e) = client.send_dmx(1, &buffer) {
+                    if let Err(e) = client.send_dmx(u32::from(universe), &buffer) {
                         error!(
                             err = e.to_string(),
                             "Error sending DMX packet to universe {}", universe
